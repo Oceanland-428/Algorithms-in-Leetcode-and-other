3c2e7dfec6d10925d5a72085d1963718dc9e809f
Oceanland-428 <33021265+Oceanland-428@users.noreply.github.com>
2018-01-15 22:25:49 -0800

    Create middleNodeOfLinkedList.java

---
+++ General/middleNodeOfLinkedList.java
@@ -0,0 +1,22 @@
+/**
+ * class ListNode {
+ *   public int value;
+ *   public ListNode next;
+ *   public ListNode(int value) {
+ *     this.value = value;
+ *     next = null;
+ *   }
+ * }
+ */
+public class Solution {
+  public ListNode middleNode(ListNode head) {
+    if (head == null) return head;
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast.next != null && fast.next.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+    }
+    return slow;
+  }
+}
